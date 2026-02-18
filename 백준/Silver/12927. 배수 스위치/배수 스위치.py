63ebdfb8bf1061ab376719a0de2dea6f8da316ab
bsw1206 <toysw1206@gmail.com>
2026-02-18 20:23:57 +0900

    [Silver IV] Title: 배수 스위치, Time: 28 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/12927. 배수 스위치/배수 스위치.py
+++ 백준/Silver/12927. 배수 스위치/배수 스위치.py
@@ -1,10 +1,11 @@
-lamp = [0] + list(input())
+# boj 12927 : 배수 스위치
+lamp = [0] + list(input()) # 배수를 맞추기 위해 [0] 추가
 count = 0
 N = len(lamp)
 for i in range(1, N):
-    if lamp[i] == 'Y':
+    if lamp[i] == 'Y': # Y 인 경우 사용 한번 함.
         count += 1    
-        for j in range(i, N, i):
+        for j in range(i, N, i): # i 번위치에서 시작, i의 배수 나열 <- 여기가 핵심인듯?
             if lamp[j] == 'Y':
                 lamp[j] = 'N'
             else:
