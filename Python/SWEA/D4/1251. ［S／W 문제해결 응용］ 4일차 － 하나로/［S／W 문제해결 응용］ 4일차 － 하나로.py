d3d9b162ca1b3555533ba3f867737d1bc2748ea2
bsw1206 <toysw1206@gmail.com>
2026-04-22 15:08:58 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 1,853 ms, Memory: 113,024 KB -BaekjoonHub

---
+++ Python/SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.py
@@ -0,0 +1,37 @@
+from heapq import heappop, heappush
+
+def prim(tax):
+
+    q = [(0, 0)]
+
+    
+    visited = [0] * (N + 1)
+    min_cost = 0
+
+    while q:
+        cost, node = heappop(q)
+
+        if visited[node]:
+            continue
+        visited[node] = 1
+        min_cost += cost
+        for next_node in range(N):
+            if visited[next_node]:
+                continue
+
+            next_cost =  (((x_lst[next_node] - x_lst[node]) ** 2) +  ((y_lst[next_node] - y_lst[node]) ** 2)) * tax
+
+            heappush(q, (next_cost, next_node))
+    return round(min_cost)
+
+        
+
+    
+T = int(input())
+for tc in range(1, T + 1):
+    N = int(input())
+    x_lst = list(map(int, input().split()))
+    y_lst = list(map(int, input().split()))
+    E = float(input())
+    min_val = prim(E)
+    print(f'#{tc} {min_val}')
